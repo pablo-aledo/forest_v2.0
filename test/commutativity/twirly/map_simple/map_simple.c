daf4c9a6cd2904d2d42a1b90ca58c9ec8ed2a5af
Pablo <pablo.aledo@gmail.com>
2017-09-26 18:01:26 +0100

    small changes to some twirly examples

--- test/commutativity/twirly/map_simple/map_simple.c
+++ test/commutativity/twirly/map_simple/map_simple.c
@@ -1,15 +1,9 @@
-
-#include <stdio.h>
-#include <stddef.h>
-
 int main() {
   unsigned array[] = {3, 5, 15, 99, 11, 33, 5, 67};
-  const size_t N = sizeof(array) / sizeof(unsigned int);
+  const int N = sizeof(array) / sizeof(unsigned int);
 
-  for (size_t i = 0; i < N; ++i)
+  for (int i = 0; i < N; ++i)
     array[i] = i;
-
-  fprintf(stderr, "%u\n", array[N / 2]);
 
   return 0;
 }
